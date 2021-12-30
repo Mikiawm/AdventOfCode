1176a18c6f2199a63a11a45997d3e9dc006562b8
Michal Sobanski <michal.sobanski@ihsmarkit.com>
2021-12-30 14:30:25 +0100

    aoc2021-day7

---
+++ AdventOfCode2021/Day7/Day7.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Common;
+
+namespace AdventOfCode2021.Day7
+{
+    public static class Day7
+    {
+        public static int GetResultPart1()
+        {
+            var fileReader = new FileReader("Day7/input.txt");
+
+            var crabPositions = fileReader.GetFileText().Split(",").Select(int.Parse);
+            var listOfSums = new List<int>();
+            for (var i = 0; i < crabPositions.Max(); i++)
+            {
+                var sum = 0;
+                foreach (var crabPosition in crabPositions)
+                {
+                    sum += Math.Abs(crabPosition - i);
+                }
+
+                listOfSums.Add(sum);
+
+            }
+            return listOfSums.Min();
+        }
+
+        public static int GetResultPart2()
+        {
+            var fileReader = new FileReader("Day7/input.txt");
+
+            var crabPositions = fileReader.GetFileText().Split(",").Select(int.Parse);
+            var listOfSums = new List<int>();
+            for (var i = 0; i < crabPositions.Max(); i++)
+            {
+                var sum = 0;
+                foreach (var crabPosition in crabPositions)
+                {
+                    sum += Enumerable.Range(1, Math.Abs(crabPosition - i)).Sum();
+                }
+
+                listOfSums.Add(sum);
+
+            }
+            return listOfSums.Min();
+        }
+    }
+}
